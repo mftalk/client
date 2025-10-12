08e7c6a78e360e4d382e9a90d2579e465efe530c
B <bb3210x@icloud.com>
2025-10-12 08:18:39 +0200

    Update generatePrimes.py

--- generatePrimes.py
+++ generatePrimes.py
@@ -40,5 +40,5 @@ def generatePrimes(bar_show_terminal=False):
         if bar_show_terminal:
             if i % 100000 == 0 or progress == 100:
                 print(f"\r{bar(progress)}", end='')
-
+    print("\n")
     return primes
