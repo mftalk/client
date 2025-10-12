77539cd5291e73fef2f9bba8b77ae82c95d6ec61
B <bb3210x@icloud.com>
2025-10-12 08:16:37 +0200

    Update generatePrimes.py

--- generatePrimes.py
+++ generatePrimes.py
@@ -37,7 +37,7 @@ def generatePrimes(bar_show_terminal=False):
 
         progress = round((i / 999999 * 10000)) / 100
 
-        if barBool:
+        if bar_show_terminal:
             if i % 100000 == 0 or progress == 100:
                 print(f"\r{bar(progress)}", end='')
 
