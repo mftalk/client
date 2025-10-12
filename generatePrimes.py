22881900860ef13a09158d8830f9dd9b31740af5
B <bb3210x@icloud.com>
2025-10-12 08:28:10 +0200

    Update generatePrimes.py

--- generatePrimes.py
+++ generatePrimes.py
@@ -12,21 +12,7 @@ def generatePrimes(bar_show_terminal=False):
             elif round(prg) < i:
                 barList.append('-')
             else:
-                match i % 7:
-                    case 0:
-                        barList.append("M")
-                    case 1:
-                        barList.append("F")
-                    case 2:
-                        barList.append("T")
-                    case 3:
-                        barList.append("a")
-                    case 4:
-                        barList.append("l")
-                    case 5:
-                        barList.append("k")
-                    case _:
-                        barList.append("-")
+                barList.append('=')
 
         barList.append(f"] {round(prg * 10) / 10} %")
         return ''.join(barList)
