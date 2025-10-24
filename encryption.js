9313aa9d589b20fe186aa586098840c357569b70
zegof <117827585+zegof@users.noreply.github.com>
2025-10-24 13:16:30 +0200

    Fixed encyption - (AmberChriffre) give now the same encrypted text in all languages - Simplified a some code in 'AmberChriffre'
    Co-authored-by: ItsGraphax <ItsGraphax@users.noreply.github.com>

--- encryption.js
+++ encryption.js
@@ -2,16 +2,18 @@ function mod(a, b) {
   return ((a % b) + b) % b;
 }
 
-
+// should be hardcoded and the same for ALL versions in all languages
 let asciiLetters = '';
-
 for (let i = 32; i <= 126; i++) {
     asciiLetters += String.fromCharCode(i);
 }
+for (let i = 62452; i<= 62976; i++) {
+    asciiLetters += String.fromCharCode(i)
+}
+console.log(asciiLetters)
 
+const ALL_LETTERS = asciiLetters;
 const INVALID = "¤";
-//const ALL_LETTERS = asciiLetters + INVALID;
-const ALL_LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890,.-;:_¦@#°§¬|¢[]{}+\"*%&/()= °~^'?`!"
 let randtick = 1;
 
 let found_letters = [];
@@ -31,7 +33,7 @@ for (const letter in ALL_LETTERS){
 function randseed(seed, max){
     let random;
     random = (Math.ceil((532 - randtick) * 2.3)) + Math.floor(seed / 3);
-    random = (random % max) + 1;
+    random = mod(random, max) + 1;
     randtick ++;
     return random;
 }
@@ -54,13 +56,13 @@ for (const _ of ALL_LETTERS){
  */
 function generate_keysum(key){
     let key_sum = 0;
-    for (let key_char in key){
+    key.split('').forEach((key_char) => {
         if (!letters.includes(key_char)) {
             key_char = INVALID;
         }
         let key_val = letters.indexOf(key_char);
         key_sum += key_val;
-    }
+    });
     return key_sum;
 }
 
@@ -73,7 +75,7 @@ function generate_keysum(key){
  */
 function base_crypt(text, key, mode){
     let key_sum = generate_keysum(key);
-    let key_i = (Math.round(key.length / 3) + key_sum) % key.length;
+    let key_i = mod((Math.round(key.length / 3) + key_sum), key.length);
     let output = "";
 
     for (let char of text) {
@@ -93,7 +95,7 @@ function base_crypt(text, key, mode){
         let encrypted_char = letters[encrypted_val];
 
         output += encrypted_char;
-        key_i = (key_i + 1) % key.length;
+        key_i = mod((key_i + 1), key.length);
     }
     return output;
 }
@@ -106,10 +108,13 @@ export function decrypt(text, key) {
     return base_crypt(text, key, -1);
 }
 
+
+
+// Example:
 const v = encrypt(
         "Hello World! This is AmberChriffre. An advanced, but simple and hard-to-crack chiffre developed by ItsGraphax, originally for AmberOS in OSWars 10 for scratch. It has now been made **even** better with v2! Am I the only one who's confused tho at why the heck theres a big G and a big D in for AmberOS and been?",
         "pizzalover122",
     );
 console.log("%c" + "encrypted:", "color:green;font-weight:bold;", v);
-console.log("%c" + "decrypted:", "color:green;font-weight:bold;", decrypt(v, "pizzalover122"));
-
+//console.log("%c" + "decrypted:", "color:green;font-weight:bold;", decrypt(v, "pizzalover122"));
+//console.log(decrypt("?=?TewW^+rB6GK+\\vCfs:D-+Ly#+#CGdrx5]VUGCLz93\\e73]+\\$`]\\Q5ee:hXBe}C#L7[m-6+GJyGE+\\:Gve:fv2L5fC=3C_y:P^Z.yCC+9]# ^+(Np7C??}Cdol',\"+.keT=Ng P\\R&9:``-=Gmr:zI9&$E+H=jq ThWeUYW(ER{g Uh$Le6!=zR3(*:dv--.k(&C[a e9|eVB`t(PV^ ^wr`eYP=(;Vm';'I)UvU]=3Cqh^'G-e>JJ(PV^ TlIye$J=#R=wa:d(Nel`r$3CZ 0kVeXG_$(:ev \"o!L9[5(r8>wbxlXT", "pizzalover122" ))
